e0e2f4dc4a84e0998f76de4a10ab29557a3dcbc0
Rafael Fonseca <rafael@divulguedireto.com.br>
2013-07-12 00:54:40 -0300

    Corrected an error

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var index = fs.readFile("index.html");
+var index = fs.readFileSync("index.html").toString();
 
 app.get('/', function(request, response) {
   response.send(index);
